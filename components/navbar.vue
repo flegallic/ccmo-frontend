f73f0c5942450f4110e4184ccbbb3bc40d3c5c13
zwvd9898 <you@example.com>
2022-06-11 23:10:33 +0200

    minor update navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,40 +1,30 @@
 <template>
+  <v-card
+    class="d-flex justify-start navbar"
+    :color="$vuetify.theme.dark ? 'blue lighten-8' : 'blue lighten-8'"
+    flat
+    tile >
     <v-card
-        class="d-flex justify-start navbar"
-        :color="$vuetify.theme.dark ? 'blue lighten-8' : 'blue lighten-8'"
-        flat
-        tile
-      >
-        <v-card
-            class="navbar-logo"
-            outlined
-            flat
-            title
-            color="transparent"
-        >
-
-            <v-img
-            max-width="120"
-            :src="require(`../static/logo.png`)"
-            ></v-img>
-        </v-card>
-        <v-card
-            class="navbar-title"
-            outlined
-            flat
-            title
-            color="transparent"
-        >
-            Cloud Cost Optimization
-        </v-card>
+      class="navbar-logo"
+      outlined
+      flat
+      title
+      color="transparent" >
+      <v-img
+      max-width="120"
+      :src="require(`../static/logo.png`)" >
+      </v-img>
     </v-card>
+    <v-card
+      class="navbar-title"
+      outlined
+      flat
+      title
+      color="transparent">
+      <p color="white" >Cloud Cost Optimization</p>
+    </v-card>
+  </v-card>
 </template>
-
-<script>
-export default {
-
-}
-</script>
 
 <style lang="scss">
 .navbar{
@@ -47,7 +37,9 @@ export default {
     background-color: transparent; border:0; padding:24px 10px 0 20px;
   }
   .navbar-title{
-    background-color: transparent; border:0; padding:60px 20px 0 10px; color:#fff
+    background-color: transparent; border:0; padding:60px 20px 0 10px; color:#fff;
+
+    p{ color: #fff;}
   }
 }
 </style>
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,14 +3,13 @@ export default function ({app, route, redirect}){
   if (route.path === '/') {
     //protected route
     if(!app.$fire.auth.currentUser) {
-      //go to sign in page
+      //redirect to signin page
       return redirect('/auth/signin')
     }
   }
   else if (route.path === '/') {
-    //protected route
     if(app.$fire.auth.currentUser) {
-      //go to sign in page
+      //redirect to root page
       return redirect('/')
     }
   }
